c9ce8239ac203a1520f28290de9903cb289cd2a7
aapopov52 <149938611+aapopov52@users.noreply.github.com>
2024-06-09 21:24:01 +0300

    для запуска gradio

---
+++ Lab_Itog/docker_data/app_model.py
@@ -0,0 +1,16 @@
+﻿import os
+import pickle
+import json
+
+def model_run_grad(s1, s2, s3, s4):
+    l_input = [int(s1), int(s2), int(s3), int(s4)]
+    return model_run(l_input)
+
+def model_run(l_input):
+    #l_input = [1,1,1,1]
+    with open('/app/iris_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    
+    return model.predict([l_input])[0]
+
+
